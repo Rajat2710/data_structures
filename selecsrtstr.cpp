fb11b395acc96c6f01736125711160069ada5bd4
Rajat2710 <rajatbansal1012@gmail.com>
2019-08-12 20:37:59 +0530

    sort a single string

--- selecsrtstr.cpp
+++ selecsrtstr.cpp
@@ -1,30 +1,30 @@
 #include<iostream>
 using namespace std;
-void selsort(char *a[],int n)
+void selsort(string a)
 {
-        for(int i=1;i<n;i++)
-        {
-                int min_i=i,j=0;
-                for(int j=i;j<n;j++)
-                {
-                        if(*a[min_i]>*a[j])
-                        {
-                        min_i=j;
-		// cout<<*a[min_i]<<min_i<<*a[j]<<" ";
-		// cout<<endl;
-                        }
-                }
-                char temp=*a[i];
-           *a[i]=*a[min_i];
-                *a[min_i]=temp;
-        }
-        for(int i=1;i<n;i++)
-                cout<<*a[i]<<" ";
+	int l=a.length();
+for(int i=0;i<l-1;i++)
+{
+	int min_i=i;
+	for(int j=i+1;j<l;j++)
+	{
+		if(a[min_i]>a[j])
+		{
+		min_i=j;
+		}
+	}
+	  int temp=a[i];
+                a[i]=a[min_i];
+                a[min_i]=temp;
+}
+for(int i=0;i<l;i++)
+cout<<a[i];
 }
 
-
-int main(int argc,char* argv[])
+int main(void)
 {
-        selsort(argv,argc);
+        string str;
+	cin>>str;
+	selsort(str);
 }
 
