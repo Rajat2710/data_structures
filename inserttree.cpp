bc7eaff407931973af1f9d442e78bcedcb55ae06
Rajat2710 <rajatbansal1012@gmail.com>
2019-11-11 01:53:42 +0530

    insert value in tree

---
+++ inserttree.cpp
@@ -0,0 +1,77 @@
+#include<iostream>
+#include<stdlib.h>
+#include<stack>
+#include<queue>
+using namespace std;
+class Node{
+	public:
+	int data;
+	Node *next[2];
+};
+Node *newNode(int data){
+	Node *node=(Node*)malloc(sizeof(Node));
+	node->data=data;
+	node->next[0]=node->next[1]=NULL;
+	return node;
+}
+Node *maketree(int arr[],Node *root,int i,int n){
+	if(i<n){
+		Node *temp=newNode(arr[i]);
+		root=temp;
+		if(arr[2*i+1]!=-1)
+			root->next[0]=maketree(arr,root->next[0],2*i+1,n);
+		if(arr[2*i+2]!=-1)
+                        root->next[1]=maketree(arr,root->next[1],2*i+2,n);
+	}
+	return root;
+}
+void insert(Node *root,int key){
+	if(root==NULL)
+		return;
+queue<Node *>q;
+q.push(root);
+while(q.empty()==false)
+{
+	Node *node=q.front();
+	q.pop();
+	if(node->next[0]!=NULL)
+		q.push(node->next[0]);
+	else{
+		node->next[0]=newNode(key);
+		break;
+	}
+	if(node->next[1]!=NULL)
+		q.push(node->next[1]);
+	else{
+		node->next[1]=newNode(key);
+		break;
+}
+}
+}
+void levelorder(Node *root){
+	if(root==NULL)
+		return;
+queue<Node *>q;
+q.push(root);
+while(q.empty()==false)
+{
+	Node *node=q.front();
+	cout<<node->data<<" ";
+	q.pop();
+	if(node->next[0]!=NULL)
+		q.push(node->next[0]);
+	if(node->next[1]!=NULL)
+		q.push(node->next[1]);
+}
+}
+int main(int argc,char *argv[]){
+	int n=argc-2;
+	int arr[n];
+	int k=atoi(argv[1]);
+	for(int i=0;i<n;i++)
+		arr[i]=atoi(argv[i+2]);
+	Node *root=maketree(arr,root,0,n);
+	levelorder(root);
+	insert(root,k);
+	levelorder(root);
+}
