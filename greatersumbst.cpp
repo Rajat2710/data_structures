df6d7e5f654fbc25d1ed295a539cfe57fd71bb2f
Rajat2710 <rajatbansal1012@gmail.com>
2019-11-14 22:59:37 +0530

    convert bst into greatest sum bst in which every node contain sum of greater element in tree

---
+++ greatersumbst.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+#include<stdlib.h>
+#include<queue>
+using namespace std;
+class Node{
+	public:
+	int data;
+	Node *next[2];
+};
+Node *newNode(int key){
+	Node *temp=new Node();
+	temp->data=key;
+	temp->next[0]=temp->next[1]=NULL;
+	return temp;
+}
+Node *constructbst(int arr[],int st,int end){
+	if(st>end)
+		return NULL;
+	int mid=(st+end)/2;
+	Node *root=newNode(arr[mid]);
+	root->next[0]=constructbst(arr,st,mid-1);
+	root->next[1]=constructbst(arr,mid+1,end);
+	return root;
+}
+void levelorder(Node *root){
+if(root==NULL)
+	return;
+queue<Node *>q;
+q.push(root);
+while(!q.empty()){
+	int count=q.size();
+	while(count>0){
+	Node *node=q.front();
+	cout<<node->data<<" ";
+	q.pop();
+	if(node->next[0]!=NULL)
+		q.push(node->next[0]);
+	if(node->next[1]!=NULL)
+		q.push(node->next[1]);
+	count--;
+	}
+	cout<<endl;
+}
+}
+void grsbst(Node *root,int *sum){
+	if(root==NULL)
+		return;
+	grsbst(root->next[1],sum);
+	*sum=*sum+root->data;
+	root->data=*sum-root->data;
+	grsbst(root->next[0],sum);
+}
+void greatersumbst(Node *root){
+	int sum=0;
+	grsbst(root,&sum);
+}
+int main(int argc,char *argv[]){
+	int n=argc-1;
+	int arr[n];
+	for(int i=0;i<n;i++){
+		arr[i]=atoi(argv[i+1]);
+	}
+	Node *root=constructbst(arr,0,n-1);
+	levelorder(root);
+	greatersumbst(root);
+	levelorder(root);
+}
